199425531d93fd6ad9c8ca9d722c2b41614b83d0
abodarev <abondarev@phoenixit.ru>
2025-01-04 22:15:07 +0300

    feat - init commit - add swiper and done page template

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,35 @@
+const feedbackSwiper = new Swiper('.feedback-slider', {
+    wrapperClass: 'feedback-wrapper',
+    slideClass: 'feedback-slide',
+    slidesPerView: "auto",
+    spaceBetween: 20,
+    loopFillGroupWithBlank: false,
+    navigation: {
+        nextEl: '.feedback-next',
+        prevEl: '.feedback-prev',
+    },
+});
 
+
+const newsSwiper = new Swiper('.news-slider', {
+    wrapperClass: 'news-wrapper',
+    slideClass: 'news-slide',
+    slidesPerView: "auto",
+    spaceBetween: 20,
+    loopFillGroupWithBlank: false,
+    navigation: {
+        nextEl: '.news-next',
+        prevEl: '.news-prev',
+    },
+});
+
+
+{
+    const faqBlock = document.querySelector('.faq');
+    const questions = faqBlock.querySelectorAll('.question');
+    questions.forEach((question) => {
+        question.addEventListener("click", () => {
+            question.classList.toggle('question_open');
+        })
+    })
+}
